9155f1e023da4ac0106f6a1f34ce7a920daa81f8
honai <29170100+honai@users.noreply.github.com>
2019-12-17 22:20:32 +0900

    env: update build dirname

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports =(env, argv) => {
     entry: './src/index.js',
     output: {
       filename: 'main.js',
-      path: path.resolve(__dirname, 'dist')
+      path: path.resolve(__dirname, 'public')
     },
     module: {
       rules: [
