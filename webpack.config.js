d312e7534bfd77e77c2cfb32f1f4fac92801d913
honai <29170100+honai@users.noreply.github.com>
2019-12-17 19:54:45 +0900

    env: update webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,34 @@
 const path = require('path')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        exclude: /node_modules/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]'
+module.exports =(env, argv) => {
+  const isDev = argv.mode === 'development'
+  const config = {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          exclude: /node_modules/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        },
+        {
+          test: /\.elm$/,
+          exclude: [/elm-stuff/, /node_modules/],
+          loader: 'elm-webpack-loader',
+          options: {
+            debug: isDev,
+            optimize: !isDev
+          }
         }
-      },
-      {
-        test: /\.elm$/,
-        exclude: [/elm-stuff/, /node_modules/],
-        loader: 'elm-webpack-loader',
-        options: {
-          debug: true
-        }
-      }
-    ]
+      ]
+    }
   }
+  return config
 }
